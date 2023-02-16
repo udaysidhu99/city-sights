a0e709e842a6ab6e8124e23b8c08dd62ee424102
Uday <udaysidhu1@outlook.com>
2023-02-16 23:07:03 +0530

    Initial Commit

---
+++ City Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City Sights
+//
+//  Created by Uday Sidhu on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City Sights/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  City Sights
+//
+//  Created by Uday Sidhu on 16/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
